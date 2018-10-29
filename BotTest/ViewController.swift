d99235bfa2e8d3a9b8683e2773472430a3d4c97c
ravi1kumar <ravi1.kumar@wynk.in>
2018-10-29 10:58:04 +0530

    Initial Commit

---
+++ BotTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BotTest
+//
+//  Created by B0208144 on 29/10/18.
+//  Copyright © 2018 B0208144. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
